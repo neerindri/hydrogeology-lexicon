09dc8061f64857a175073fc191f9d098779aecbb
neerindri <neerindri@outlook.com>
2024-09-02 20:16:29 +0200

    working table version

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 let Lexikon;
 function getLexikon() {
-    return fetch('./lexikon.json')
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json(); // Returns a promise that resolves with JSON data
-        }).then((data) => {Lexikon = Object.values(data)});
+  return fetch("./lexikon.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json(); // Returns a promise that resolves with JSON data
+    })
+    .then((data) => {
+      Lexikon = Object.values(data);
+    });
 }
 // const Lexikon = fetch('lexikon.json').then(response => response.json).then(data => console.log(data))
 // const Lexikon = getLexikon()
 getLexikon();
 
 const searchTerm = (row, lang, term) => {
-    return row[lang].includes(term)
-}
+  return row[lang].includes(term);
+};
 const search = () => {
-    const lang = document.getElementById('language').value
-    const term = document.getElementById('search-term').value.toLowerCase()
-    var resultTable= document.getElementById('results')
-    resultTable.innerHTML = ''
-    
+  const lang = document.getElementById("language").value;
+  const term = document.getElementById("search-term").value.toLowerCase();
+  var resultTableBody = document.getElementById("results");
+  resultTableBody.innerHTML = "";
 
-    console.log('Got search values', term, lang)
+  console.log("Got search values", term, lang);
 
-    result = Lexikon.filter((row) => searchTerm(row,lang, term))
-    console.log('Search result', result)
-    if (result.length > 0) {
-        result.forEach(element => {
-            resultTable.innerHTML += (`<p>German:${element.de}</p>` +
-            `<p>English:${element.en}</p>` +
-`<p>French:${element.fr}</p>` +
-`<p>Spanish:${element.es}</p>`)
-        });
-    } else {
-        resultTable.innerHTML = '<p>No results found for search term</p>'
-    }
-}
-const searchButton = document.getElementById('search-button')
+  results = Lexikon.filter((row) => searchTerm(row, lang, term));
+  console.log("Search result", results);
+  if (results.length > 0) {
+    results.forEach((result) => {
+      const row = document.createElement("tr");
+      for (const key in result) {
+        const cell = document.createElement("td");
+        cell.textContent = result[key];
+        row.appendChild(cell);
+      }
+      resultTableBody.appendChild(row);
+    });
+    resultTableBody.innerHTML += "</tbody></table>";
+  } else {
+    resultTableBody.innerHTML = "<p>No results found for search term</p>";
+  }
+};
+const searchButton = document.getElementById("search-button");
 
-searchButton.addEventListener('click', search)
+searchButton.addEventListener("click", search);
